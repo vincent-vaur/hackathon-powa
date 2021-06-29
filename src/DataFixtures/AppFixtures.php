6ccd5fb63d11a87cab363f1116ea9e3bb1eb6e35
BURTY Quentin <quentin.burty@gmail.com>
2021-06-29 15:23:43 +0200

    users fixtures added

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -6,45 +6,26 @@ use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
+use Faker\Generator;
+use Faker;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+        $faker = Faker\Factory::create("fr_FR");
+        
 
 
-        $users = [
-            [
-                'firstname' => 'Vincent',
-                'lastname' => 'Vaur',
-                'age' => 25,
-                'job' => 'Developer',
-            ],
-            [
-                'firstname' => 'Anthony',
-                'lastname' => 'Deplanque',
-                'age' => 24,
-                'job' => 'Developer',
-            ],
-            [
-                'firstname' => 'Chloé',
-                'lastname' => 'PrendreCher',
-                'age' => 26,
-                'job' => 'Developer',
-            ]
-        ];
-
-        foreach ($users as $userData) {
-            $user = new User();
-            $user
-                ->setFirstname($userData['firstname'])
-                ->setLastname($userData['lastname'])
-                ->setAge($userData['age'])
-                ->setJob($userData['job']);
-
-                $manager->persist($user);
+        for ($i = 0; $i <= 2000; $i++) {
+            $users = new User;
+            $users->setFirstname($faker->firstName());
+            $users->setLastname($faker->lastName());
+            $users->setAge($faker->numberBetween(18, 70));
+            $users->setJob($faker->jobTitle());
+            $manager->persist($users);
         }
-
         $manager->flush();
+
     }
 }
