7a44c44bf031aa8e3e6bda05f184404d9a009493
SebastienFrancois <sebastien.devlopweb@gmail.com>
2021-06-29 15:59:45 +0200

    Pas grave je te pardonne

--- assets/app.js
+++ assets/app.js
@@ -1,51 +1,21 @@
-<<<<<<< HEAD
-import React, { Component} from "react";
+import React, { Component } from "react";
 import ReactDom from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import LayoutBoard from "./components/LayoutBoard";
+import GlobalContext from "./contexts/GlobalContext";
 
 class App extends Component {
   render() {
-    return (
-      <>
-        <LayoutBoard />
-=======
-import React, { Component } from 'react';
-import  ReactDom  from 'react-dom';
-import './app.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Board from './components/Board';
-import Navigation from './components/Navigation';
-import logo from './img/fiverr.png';
-import GlobalContext from './contexts/GlobalContext';
-import Login from './components/Login';
-
-class App extends Component {
-  render() {
-
     return (
       <>
         <GlobalContext>
-          <div className='logo'>
-            <img src={logo} alt='logo' />
-          </div>
-          <div className='Login'>
-            <Login />
-          </div>
-          <div className='App'>
-            <Navigation />
-            <Board />
-          </div>
+          <LayoutBoard />
         </GlobalContext>
->>>>>>> 67203152dbbee75dc5c3250338b14b27e16f81a3
       </>
     );
   }
-};
+}
 
-ReactDom.render(<App />, document.getElementById('root'));
+ReactDom.render(<App />, document.getElementById("root"));
 
 
-<div className='Login'>
-            <Login />
-          </div>
