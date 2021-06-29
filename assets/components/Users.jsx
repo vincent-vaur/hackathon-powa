e3d0b722f359268d850fb26f0aa10e5a61c50562
Léa Mahler <leamahler85@gmail.com>
2021-06-29 09:39:06 +0200

    Serializer 1

--- assets/components/Users.jsx
+++ assets/components/Users.jsx
@@ -24,7 +24,7 @@ export default Users => {
         <>
             <h1>Users</h1>
             {users.map(user => (
-                <p>{ user.firstname } { user.lastname }, { user.age }</p>
+                <p>{ user.firstname } { user.lastname }</p>
             ))}
         </>
     )
--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -4,8 +4,21 @@ namespace App\Controller;
 
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+
+$encoders = [new XmlEncoder(), new JsonEncoder()];
+$normalizers = [new ObjectNormalizer()];
+
+$serializer = new Serializer($normalizers, $encoders);
+
+
+
 
 /**
  * @Route("/api", name="api_")
@@ -16,8 +29,11 @@ class ApiController extends AbstractController
     /**
      * @Route("/user", name="user")
      */
-    public function index(UserRepository $userRepository): Response
+    public function index(UserRepository $userRepository, Serializer $serializer): JsonResponse
     {
+        $jsonContent = $serializer->serialize($userRepository->findAll, 'json');
+
+        echo $this->json($userRepository->find());exit;
         return $this->json($userRepository->findAll());
     }
 }
