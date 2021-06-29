690d48d296fd489a06aec765f44cad8b8f3952f1
BURTY Quentin <quentin.burty@gmail.com>
2021-06-29 14:33:33 +0200

    almost faker

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,11 +5,14 @@ namespace App\DataFixtures;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+
+
         $users = [
             [
                 'firstname' => 'Vincent',
